0c8dc2970892bd00beab33a595958759c711697e
Daniel Lucas Ferreira <daniel.ferreira2675@gmail.com>
2022-09-26 14:39:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route} from "react-router-dom"
+import { Container} from 'react-bootstrap'
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { Home } from "./pages/Home"
+import { Navbar} from "./components/Navbar"
+
+//1020
+
+function App() {
+return (
+<>
+<Navbar />
+ <Container className="mb-4">
+  <Routes>
+    <Route path="/" element= { <Home/> } />
+    <Route path="/store" element= { <Store/> } />
+    <Route path="/about" element= { <About/> } />
+  </Routes>
+</Container>
+</>
+)
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Container, Navbar as NavbarBs } from "react-bootstrap"
+
+export function Navbar() {
+    return (
+    <NavbarBs className="bg-white shadow-sm mb-3">
+    <Container>Nav</Container>
+    </NavbarBs>
+    )
+}
