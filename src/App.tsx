5cbd2d2b0a11d342337b53d4ecca9f30df3e2fb4
Daniel Lucas Ferreira <daniel.ferreira2675@gmail.com>
2022-10-04 13:09:50 -0300

    in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import { Routes, Route} from "react-router-dom"
-import { Container} from 'react-bootstrap'
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 import { About } from "./pages/About"
-import { Home } from "./pages/Home"
-import { Navbar} from "./components/Navbar"
+import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
-//2920
 
-//Erro em components/StoreItems
 
 function App() {
-return (
-<>
-<Navbar />
- <Container className="mb-4">
-  <Routes>
-    <Route path="/" element= { <Home/> } />
-    <Route path="/store" element= { <Store/> } />
-    <Route path="/about" element= { <About/> } />
-  </Routes>
-</Container>
-</>
-)
+  return (
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  )
 }
+
+
 
 export default App
